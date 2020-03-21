ffe9cbe70e14bbcdd112e761d6702ec7db6018bc
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-03-21 17:51:58 -0500

    created site styles theme

---
+++ frontend/components/styles/theme.js
@@ -0,0 +1,10 @@
+import mediaQueries from './mediaQueries';
+import colors from './colors';
+
+const theme = {
+  ...colors,
+  ...mediaQueries,
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
+
+export default theme;
