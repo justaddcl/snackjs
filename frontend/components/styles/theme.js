63a5702175a78219fe589733e0f5e4ecfdd7c1d1
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-03-21 18:18:47 -0500

    refactored theme to access theme variables as objects

--- frontend/components/styles/theme.js
+++ frontend/components/styles/theme.js
@@ -2,8 +2,8 @@ import mediaQueries from './mediaQueries';
 import colors from './colors';
 
 const theme = {
-  ...colors,
-  ...mediaQueries,
+  colors,
+  mediaQueries,
   bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
 };
 
