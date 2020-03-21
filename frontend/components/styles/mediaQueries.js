b7406196369f4968f66f7d62b85d23966a308b7d
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-03-21 17:50:14 -0500

    added media queries

---
+++ frontend/components/styles/mediaQueries.js
@@ -0,0 +1,9 @@
+const mediaQueries = {
+  mqMobile: '376px',
+  mqTablet: '768px',
+  mqDesktopSmall: '1024px',
+  mqDesktopMedium: '1280px',
+  mqDesktopLarge: '1440px',
+};
+
+export default mediaQueries;
