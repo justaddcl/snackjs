d00ebd9bc9e4a08e23b60facdbc64cf157ab3e0a
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-03-21 22:50:01 -0500

    added font and layout variables to theme

--- frontend/components/styles/theme.js
+++ frontend/components/styles/theme.js
@@ -1,8 +1,12 @@
-import mediaQueries from './mediaQueries';
 import colors from './colors';
+import font from './font';
+import layout from './layout';
+import mediaQueries from './mediaQueries';
 
 const theme = {
   colors,
+  font,
+  layout,
   mediaQueries,
   bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
 };
