32b1a1e610373f6c072a7b4b2fd1d1a179cb92ec
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-03-21 22:54:50 -0500

    added events and small groups links to nav

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -2,12 +2,20 @@ import Link from 'next/link';
 
 const Nav = props => (
   <div>
-    <Link href="/">
-      <a>Home</a>
-    </Link>
-    <Link href="/smallgroups">
-      <a>Small Groups</a>
-    </Link>
+    <nav className="nav">
+      <ul className="nav__menu">
+        <li className="nav__item">
+          <Link href="/events">
+            <a>Events</a>
+          </Link>
+        </li>
+        <li className="nav__item nav__item--ml">
+          <Link href="/smallgroups">
+            <a>Small Groups</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
   </div>
 );
 
