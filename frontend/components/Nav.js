625ae069619830be93817a7c0588689a18989a8e
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-03-26 22:15:00 -0500

    nav - added styling

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,25 +1,68 @@
 import Link from 'next/link';
 import styled from 'styled-components';
+import theme from './styles/theme';
 
 const StyledNav = styled.nav`
+  display: none;
+  height: inherit;
+
+  .nav-menu {
+    align-items: stretch;
+    display: flex;
+    height: inherit;
+  }
+
+  .nav__item {
+    display: grid;
+    place-items: stretch start;
+
+    &:hover:after {
+      animation: hover-underline 0.3s cubic-bezier(0.68, 0, 0.265, 1.3);
+      background: linear-gradient(135deg, #240754, #7b93db);
+      /* box-shadow: inset 0 -2px 0 0 #111; */
+      content: '';
+      display: block;
+      height: 4px;
+      margin-top: -4px;
+      width: 100%;
+    }
+
+    &.ml {
+      margin-left: 24px;
+    }
+  }
+
+  .link {
+    color: ${theme.colors.blueGrey[700]};
+    font-size: 14px;
+    font-weight: 700;
+    padding: 30px 12px;
+    text-decoration: none;
+
+    &:hover {
+      color: ${theme.colors.blueGrey[900]};
+    }
+  }
+
+  @media screen and (min-width: ${theme.mediaQueries.desktop.small}) {
+    display: block;
+  }
 `;
 
 const Nav = props => (
   <StyledNav>
-    <nav className="nav">
-      <ul className="nav__menu">
-        <li className="nav__item">
-          <Link href="/events">
-            <a>Events</a>
-          </Link>
-        </li>
-        <li className="nav__item nav__item--ml">
-          <Link href="/smallgroups">
-            <a>Small Groups</a>
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <ul className="nav-menu">
+      <li className="nav__item">
+        <Link href="/events">
+          <a className="link">Events</a>
+        </Link>
+      </li>
+      <li className="nav__item ml">
+        <Link href="/smallgroups">
+          <a className="link">Small Groups</a>
+        </Link>
+      </li>
+    </ul>
   </StyledNav>
 );
 
