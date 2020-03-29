ecdffbf9e181c59501e3669f47fe159671c47df0
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-03-29 16:01:55 -0500

    user avatar - destructured props

--- frontend/components/UserAvatar.js
+++ frontend/components/UserAvatar.js
@@ -8,11 +8,12 @@ const StyledUserAvatar = styled.img`
   width: ${props => `${props.size}px` || '64px'};
 `;
 
-const UserAvatar = props => (
+const UserAvatar = ({ size }) => (
   <StyledUserAvatar
-    src={`https://source.unsplash.com/collection/8470962/${props.size}x${props.size}`}
+    src={`https://source.unsplash.com/collection/8470962/${size}x${size}`}
     alt="User avatar"
     size={props.size}
+    size={size}
   />
 );
 
