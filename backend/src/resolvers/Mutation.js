9e84f48520eeed6d1113a2f56218a9c6f23bf138
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-04-04 19:05:33 -0500

    added createUser mutation to resolvers

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,5 +1,14 @@
 const Mutation = {
+  async createUser(parent, args, ctx, info) {
+    const user = await ctx.db.mutation.createUser(
+      {
+        ...args,
+      },
+      info
+    );
 
+    return user;
+  },
 };
 
 module.exports = Mutation;
