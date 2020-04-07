696351f5b6be71f85a101ea8e5d4e44b5ddce7c1
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-04-06 21:50:18 -0500

    added small groups query to resolvers

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,6 +3,7 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
   users: forwardTo('db'),
   userRoles: forwardTo('db'),
+  smallGroups: forwardTo('db'),
 };
 
 module.exports = Query;
