29469276314959e3f73154c4a85a31caeb79d7ed
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-04-12 01:26:56 -0500

    added user query to resolver

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,6 +1,7 @@
 const { forwardTo } = require('prisma-binding');
 
 const Query = {
+  user: forwardTo('db'),
   users: forwardTo('db'),
   userRoles: forwardTo('db'),
   smallGroups: forwardTo('db'),
