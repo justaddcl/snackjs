2368b84bedf75fd5f8b4d6efb0134504692706a6
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-04-12 15:44:01 -0500

    rename smallgroups page to snake case, added page title

--- frontend/pages/small_groups.js
+++ frontend/pages/small_groups.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import styled from 'styled-components';
 import ContentContainer from '../components/styles/ContentContainer';
 
@@ -6,6 +7,9 @@ const SmallGroupsPage = styled(ContentContainer)``;
 
 const Smallgroups = props => (
   <SmallGroupsPage>
+    <Head>
+      <title>Snack App | Small Groups</title>
+    </Head>
     <p>All the groups!</p>
   </SmallGroupsPage>
 );
