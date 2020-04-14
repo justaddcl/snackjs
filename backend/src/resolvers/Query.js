d524240089189a46771a52b7d5c3b6ec722ffc5f
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-04-13 22:38:18 -0500

    added usersConnection query to resolver

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,6 +3,7 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
   user: forwardTo('db'),
   users: forwardTo('db'),
+  usersConnection: forwardTo('db'),
   userRoles: forwardTo('db'),
   smallGroups: forwardTo('db'),
   discipleshipCommunities: forwardTo('db'),
