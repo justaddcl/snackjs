95bf54e88cfbdee165e50db4e5960c3b5785e151
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-04-17 00:08:00 -0500

    added signin mutation to resolver

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -51,6 +51,28 @@ const Mutation = {
       info
     );
   },
+  async signin(parent, { data: { email, password } }, ctx, info) {
+    const user = await ctx.db.query.user({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new Error(`No user found for email ${email}`);
+    }
+
+    const valid = await bcrypt.compare(password, user.password);
+
+    if (!valid) {
+      throw new Error('Invalid password');
+    }
+
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    ctx.response.cookie('_snackjs_session', token, {
+      httpOnly: true,
+      maxAge: MS_IN_A_YEAR,
+    });
+    return user;
+  },
   async createUserRole(parent, args, ctx, info) {
     const userRole = await ctx.db.mutation.createUserRole(
       {
