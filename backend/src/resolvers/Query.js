894663a60ce706953c672c7a0fa43ef6f950a800
Yuji Nelson <k.yuji.nelson@gmail.com>
2020-04-17 00:04:15 -0500

    added current user query to resolver

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,6 +1,12 @@
 const { forwardTo } = require('prisma-binding');
 
 const Query = {
+  currentUser(parent, args, ctx, info) {
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user({ where: { id: ctx.request.userId } }, info);
+  },
   user: forwardTo('db'),
   users: forwardTo('db'),
   usersConnection: forwardTo('db'),
